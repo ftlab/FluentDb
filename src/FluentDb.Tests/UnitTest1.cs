59889fa2b420c036f90c926229caa1799e2eba83
ulyanov yuriy <ulyanov.yuriy@live.com>
2017-10-24 15:28:54 +0300

    unit test proj

---
+++ src/FluentDb.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentDb.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
